392c370bb7c3ddf593a01227913c48fb377743c9
zhao lin <lizhao@thoughtworks.com>
2019-01-31 20:12:36 +0800

    add login success test

--- app/src/test/java/com/thoughtworks/awesomesocialapp/LoginFragmentTest.java
+++ app/src/test/java/com/thoughtworks/awesomesocialapp/LoginFragmentTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.awesomesocialapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.test.filters.SmallTest;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.EditText;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.Shadows;
 import org.robolectric.shadows.support.v4.SupportFragmentController;
 
 import static org.junit.Assert.*;
@@ -16,38 +18,53 @@ import static org.junit.Assert.*;
 @RunWith(RobolectricTestRunner.class)
 @SmallTest
 public class LoginFragmentTest {
+    private static final String DEFAULT_USERNAME = "behring";
+    private static final String DEFAULT_PASSWORD = "111111";
+
+    private LoginActivity activity = getActivity();
 
     @Test
     public void onCreateView_shouldDisplayUserNameEditText() {
-        View rootView = getRootView();
-        assertNotNull(rootView);
-        View usernameEditText = rootView.findViewById(R.id.username_input);
+        View usernameEditText = getRootView().findViewById(R.id.username_input);
         assertTrue(usernameEditText instanceof EditText);
         assertEquals(usernameEditText.getVisibility(), View.VISIBLE);
     }
 
     @Test
     public void onCreateView_shouldDisplayPasswordEditText() {
-        View rootView = getRootView();
-        assertNotNull(rootView);
-        View usernameEditText = rootView.findViewById(R.id.password_input);
+        View usernameEditText = getRootView().findViewById(R.id.password_input);
         assertTrue(usernameEditText instanceof EditText);
         assertEquals(usernameEditText.getVisibility(), View.VISIBLE);
     }
 
     @Test
     public void onCreateView_shouldDisplayLoginButton() {
-        View rootView = getRootView();
-        assertNotNull(rootView);
-        View loginButton = rootView.findViewById(R.id.login_button);
+        View loginButton = getRootView().findViewById(R.id.login_button);
         assertTrue(loginButton instanceof Button);
         assertEquals(loginButton.getVisibility(), View.VISIBLE);
     }
 
+    @Test
+    public void onClickLoginButton_shouldLoginSuccessToMainActivity() {
+        ((EditText) getRootView().findViewById(R.id.username_input)).setText(DEFAULT_USERNAME);
+        ((EditText) getRootView().findViewById(R.id.password_input)).setText(DEFAULT_PASSWORD);
+        getRootView().findViewById(R.id.login_button).performClick();
+        Intent expectedIntent = new Intent(activity, MainActivity.class);
+        Intent actualIntent = Shadows.shadowOf(activity).getNextStartedActivity();
+        assertEquals(expectedIntent.getComponent(), actualIntent.getComponent());
+    }
+
     private View getRootView() {
-        LoginFragment loginFragment = SupportFragmentController
+        return setupLoginFragment().getView();
+    }
+
+    private LoginFragment setupLoginFragment() {
+        return SupportFragmentController
                 .of(LoginFragment.newInstance(), LoginActivity.class)
-                .create(R.id.fragment_container, new Bundle()).start().get();
-        return loginFragment.getView();
+                .create(R.id.fragment_container, new Bundle()).start().resume().get();
+    }
+
+    private LoginActivity getActivity() {
+        return (LoginActivity) setupLoginFragment().getActivity();
     }
 }
