ea0e183f16ee8cbc6c3aa22325a2d9cd615a1273
zhao lin <lizhao@thoughtworks.com>
2019-01-31 15:54:36 +0800

    add test for login frgment what having a username Edittext when oncreateview

---
+++ app/src/test/java/com/thoughtworks/awesomesocialapp/LoginFragmentTest.java
@@ -0,0 +1,31 @@
+package com.thoughtworks.awesomesocialapp;
+
+import android.os.Bundle;
+import android.support.test.filters.SmallTest;
+import android.view.View;
+import android.widget.EditText;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.support.v4.SupportFragmentController;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+@SmallTest
+public class LoginFragmentTest {
+
+    @Test
+    public void onCreateView_shouldDisplayUserNameEditText() {
+        LoginFragment loginFragment = SupportFragmentController
+                .of(LoginFragment.newInstance(), LoginActivity.class)
+                .create(R.id.fragment_container, new Bundle()).start().get();
+
+        View rootView = loginFragment.getView();
+        assertNotNull(rootView);
+        View usernameEditText = rootView.findViewById(R.id.username_input);
+        assertTrue(usernameEditText instanceof EditText);
+        assertEquals(usernameEditText.getVisibility(), View.VISIBLE);
+    }
+}
