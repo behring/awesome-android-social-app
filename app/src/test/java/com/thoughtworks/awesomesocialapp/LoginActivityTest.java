1983f0f113f04f3d1eabaa149ff89f77e42a41e0
zhao lin <lizhao@thoughtworks.com>
2019-01-31 13:36:08 +0800

    add login fragment and empty LoginActivityTest file

---
+++ app/src/test/java/com/thoughtworks/awesomesocialapp/LoginActivityTest.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.awesomesocialapp;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+public class LoginActivityTest {
+
+}
