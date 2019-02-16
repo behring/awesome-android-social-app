d99fe76c31b244dd832472218f01e8a85419b304
behring <lizhao@thoughtworks.com>
2019-02-16 23:08:27 +0800

    update docker contain network to jenkins alias

--- app/src/main/java/com/thoughtworks/awesomesocialapp/constants/NetworkConstants.java
+++ app/src/main/java/com/thoughtworks/awesomesocialapp/constants/NetworkConstants.java
@@ -1,7 +1,7 @@
 package com.thoughtworks.awesomesocialapp.constants;
 
 public class NetworkConstants {
-    public static String BASE_URL = "http://10.0.2.2:5000/";
+    public static String BASE_URL = "http://jenkins:5000/";
 
     public static class Code {
         public static int SUCCESS = 0;
