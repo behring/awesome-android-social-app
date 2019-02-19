d40cab061577e90ef357491af5404977072f7395
behring <lizhao@thoughtworks.com>
2019-02-19 22:21:04 +0800

    add local sonarqube

--- app/src/main/java/com/thoughtworks/awesomesocialapp/constants/NetworkConstants.java
+++ app/src/main/java/com/thoughtworks/awesomesocialapp/constants/NetworkConstants.java
@@ -5,16 +5,7 @@ import com.thoughtworks.awesomesocialapp.BuildConfig;
 public class NetworkConstants {
     public static final String BASE_URL = BuildConfig.BASE_URL;
 
-    private NetworkConstants() {
-
-    }
-
     public static class Code {
         public static final int SUCCESS = 0;
-
-        private Code() {
-
-        }
-
     }
 }
