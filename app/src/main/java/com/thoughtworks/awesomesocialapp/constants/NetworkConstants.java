2930da02d04ecf72b9d7230d83a2bd21157a67fd
zhao lin <lizhao@thoughtworks.com>
2019-02-19 16:23:36 +0800

    fix pmd

--- app/src/main/java/com/thoughtworks/awesomesocialapp/constants/NetworkConstants.java
+++ app/src/main/java/com/thoughtworks/awesomesocialapp/constants/NetworkConstants.java
@@ -3,17 +3,18 @@ package com.thoughtworks.awesomesocialapp.constants;
 import com.thoughtworks.awesomesocialapp.BuildConfig;
 
 public class NetworkConstants {
+    public static final String BASE_URL = BuildConfig.BASE_URL;
+
     private NetworkConstants() {
 
     }
 
-    public static final String BASE_URL = BuildConfig.BASE_URL;
-
     public static class Code {
+        public static final int SUCCESS = 0;
+
         private Code() {
 
         }
 
-        public static final int SUCCESS = 0;
     }
 }
