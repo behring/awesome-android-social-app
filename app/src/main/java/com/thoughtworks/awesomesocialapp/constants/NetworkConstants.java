c9f83cfc1b505fd5085d96c1aec265d281c6cc92
zhao lin <lizhao@thoughtworks.com>
2019-02-19 16:04:51 +0800

    fix code smells debt on sonar

--- app/src/main/java/com/thoughtworks/awesomesocialapp/constants/NetworkConstants.java
+++ app/src/main/java/com/thoughtworks/awesomesocialapp/constants/NetworkConstants.java
@@ -3,9 +3,17 @@ package com.thoughtworks.awesomesocialapp.constants;
 import com.thoughtworks.awesomesocialapp.BuildConfig;
 
 public class NetworkConstants {
+    private NetworkConstants() {
+
+    }
+
     public static final String BASE_URL = BuildConfig.BASE_URL;
 
     public static class Code {
+        private Code() {
+
+        }
+
         public static final int SUCCESS = 0;
     }
 }
