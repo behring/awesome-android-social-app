a0511972e79cfa43777d14d8450c7e5a8a736359
behring <lizhao@thoughtworks.com>
2019-02-20 23:08:24 +0800

    add recylerview for chatsfragment(no finish)

--- app/src/main/java/com/thoughtworks/awesomesocialapp/chats/ChatsFragment.java
+++ app/src/main/java/com/thoughtworks/awesomesocialapp/chats/ChatsFragment.java
@@ -4,23 +4,32 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.thoughtworks.awesomesocialapp.R;
+import com.thoughtworks.awesomesocialapp.databinding.FragmentChatsBinding;
 
 public class ChatsFragment extends Fragment {
 
     public static ChatsFragment newInstance() {
         return new ChatsFragment();
     }
+    private FragmentChatsBinding binding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        binding = FragmentChatsBinding.inflate(inflater);
+        initUI();
+        return binding.getRoot();
+    }
+
+    private void initUI() {
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
     @Override
