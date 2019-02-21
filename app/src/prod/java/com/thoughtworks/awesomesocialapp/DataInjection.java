43465c58f87cf0d71ae46978fb4f1a81888055ef
zhao lin <lizhao@thoughtworks.com>
2019-02-21 20:23:47 +0800

    fix flovor compile problem

--- app/src/prod/java/com/thoughtworks/awesomesocialapp/DataInjection.java
+++ app/src/prod/java/com/thoughtworks/awesomesocialapp/DataInjection.java
@@ -1,5 +1,12 @@
 package com.thoughtworks.awesomesocialapp;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.thoughtworks.awesomesocialapp.data.Repository;
+import com.thoughtworks.awesomesocialapp.data.local.AppDatabase;
+import com.thoughtworks.awesomesocialapp.network.ServerApi;
+
 public class DataInjection {
     public static Repository provideRepository(@NonNull Context context) {
         AppDatabase database = AppDatabase.getInstance(context);
--- script/check.sh
+++ script/check.sh
@@ -6,9 +6,9 @@ cd ../server && ./run.sh &
 cd ..
 
 ./gradlew --stop
-./gradlew sonarqube -Dsonar.branch=master -Dsonar.host.url=http://localhost:9001
-./gradlew clean pmd findbugs checkstyle lint testDebugUnitTest connectedAndroidTest
-
+./gradlew sonarqube -Dsonar.branch=chatsfragment -Dsonar.host.url=http://localhost:9001
+#./gradlew clean pmd findbugs checkstyle lintProdDebug testProdDebugUnitTest connectedProdDebugAndroidTest
+./gradlew clean pmd findbugs checkstyle lintMockDebug testMockDebugUnitTest connectedMockDebugAndroidTest
 # kill emulator
 kill $(lsof -t -i:5554)
 adb kill-server
