259f56f7ccd14a5a6f66872353c8ee61de6b16c4
behring <lizhao@thoughtworks.com>
2019-02-23 22:10:15 +0800

    modify retrofit singleton for gson parse str date to Date model

--- app/src/main/java/com/thoughtworks/awesomesocialapp/network/RetrofitHelper.java
+++ app/src/main/java/com/thoughtworks/awesomesocialapp/network/RetrofitHelper.java
@@ -1,19 +1,30 @@
 package com.thoughtworks.awesomesocialapp.network;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.awesomesocialapp.constants.NetworkConstants;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public final class RetrofitHelper {
-    private static Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(NetworkConstants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
+final class RetrofitHelper {
+    private static Retrofit retrofit;
 
     private RetrofitHelper() {
     }
 
-    public static Retrofit getRetrofit() {
+    static Retrofit getRetrofit() {
+        synchronized (Retrofit.class) {
+            if (retrofit == null) {
+                Gson gson = new GsonBuilder()
+                        .setDateFormat(NetworkConstants.DATE_FORMAT)
+                        .create();
+
+                retrofit = new Retrofit.Builder()
+                        .baseUrl(NetworkConstants.BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create(gson))
+                        .build();
+            }
+        }
         return retrofit;
     }
 }
